cf9e385f958e87260db220348a7dad8bea462230
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-07 15:15:20 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
-import dlib
+pip install dlib-19.19.0-cp37-cp37m-win_amd64.whl
+pip install cmake
+pip install face_recognition
 import face_recognition
 from df_change import df_change
 
