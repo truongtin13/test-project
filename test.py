957bd0882dd55ba0dc60e67884cb4824bdfd4020
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-08 10:50:04 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from mtcnn import MTCNN
+from mtcnn.mtcnn import MTCNN
+import cv2
 import numpy as np
 from scipy.spatial.distance import cosine
 from df_change import df_change
