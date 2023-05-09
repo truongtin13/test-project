944d408f486fd4e9f6120f3d3104dcdebe563fb0
truongtin13 <125672353+truongtin13@users.noreply.github.com>
2023-05-09 08:16:53 +0700

    Update app2.py

---
+++ app2.py
@@ -0,0 +1,19 @@
+import re
+import numpy as np
+
+def no_vietnamese(s):
+    s = re.sub('[áàảãạăắằẳẵặâấầẩẫậ]', 'a', s)
+    s = re.sub('[ÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ]', 'A', s)
+    s = re.sub('[éèẻẽẹêếềểễệ]', 'e', s)
+    s = re.sub('[ÉÈẺẼẸÊẾỀỂỄỆ]', 'E', s)
+    s = re.sub('[óòỏõọôốồổỗộơớờởỡợ]', 'o', s)
+    s = re.sub('[ÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ]', 'O', s)
+    s = re.sub('[íìỉĩị]', 'i', s)
+    s = re.sub('[ÍÌỈĨỊ]', 'I', s)
+    s = re.sub('[úùủũụưứừửữự]', 'u', s)
+    s = re.sub('[ÚÙỦŨỤƯỨỪỬỮỰ]', 'U', s)
+    s = re.sub('[ýỳỷỹỵ]', 'y', s)
+    s = re.sub('[ÝỲỶỸỴ]', 'Y', s)
+    s = re.sub('đ', 'd', s)
+    s = re.sub('Đ', 'D', s)
+    return ''.join(s.lower().split())
